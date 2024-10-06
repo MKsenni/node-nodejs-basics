6b76edbe7581c7d32e14417dc96fff0b383e2e79
Kseniya <k.merkulova@iqdev.digital>
2024-10-06 21:43:01 +0500

    feat: add list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,27 @@
+import { readdir } from 'node:fs';
+import path from 'path';
+const __dirname = import.meta.dirname;
+
 const list = async () => {
-    // Write your code here 
+  const filePath = path.join(__dirname, '/files');
+
+  readdir(filePath, { withFileTypes: true }, (err, files) => {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        throw new Error('FS operation failed');
+      } else {
+        throw err;
+      }
+    }
+    const arrFiles = files.map((file) => {
+      if (file.isFile()) {
+        return file.name;
+      } else {
+        throw new Error(`${file.name} is not file`);
+      }
+    })
+    console.log(arrFiles);
+  })
 };
 
 await list();
