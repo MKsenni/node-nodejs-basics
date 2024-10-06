c27076c78c7406d3de7e3cece893b4340fb52957
Kseniya <k.merkulova@iqdev.digital>
2024-10-06 22:29:24 +0500

    feat: add args function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,18 @@
+import { argv } from 'node:process';
+
 const parseArgs = () => {
-    // Write your code here 
+  const envArguments = argv.slice(2);
+  const argsInConsole = envArguments.map((argument, idx) => {
+    let value = envArguments[idx + 1];
+    if (argument.startsWith('--') && value && !value.startsWith('--')) {
+      const name = argument.replace('--', '');
+      let value = envArguments[idx + 1];
+      if (name && value) {
+        return `${name} is ${value}`;
+      }
+    }
+  }).filter((item) => item).join(', ');
+  console.log(argsInConsole);
 };
 
 parseArgs();
