35fa69ad8c2f8f23245df2d7c7760e6a397292e8
Kseniya <k.merkulova@iqdev.digital>
2024-10-06 21:35:57 +0500

    feat: add delete function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,19 @@
+import { unlink } from 'node:fs';
+import path from 'path';
+const __dirname = import.meta.dirname;
+
 const remove = async () => {
-    // Write your code here 
+  const filePath = path.join(__dirname, '/files/fileToRemove.txt');
+
+  unlink(filePath, (err) => {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        throw new Error('FS operation failed');
+      } else {
+        throw err;
+      }
+    }
+  })
 };
 
 await remove();
