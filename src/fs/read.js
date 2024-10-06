15945db89b8bfddf2c82c7a39097f0073e5a00f8
Kseniya <k.merkulova@iqdev.digital>
2024-10-06 21:45:22 +0500

    feat: add read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,20 @@
+import { readFile } from 'node:fs';
+import path from 'path';
+const __dirname = import.meta.dirname;
+
 const read = async () => {
-    // Write your code here 
+  const filePath = path.join(__dirname, '/files/fileToRead.txt');
+
+  readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        throw new Error('FS operation failed');
+      } else {
+        throw err;
+      }
+    }
+    console.log(data);
+  })
 };
 
 await read();
