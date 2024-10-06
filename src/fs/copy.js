f9e2d606ce2774058de02ae43ee0993fb4c4d562
Kseniya <k.merkulova@iqdev.digital>
2024-10-06 16:58:09 +0500

    feat: add create and copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,42 @@
+import { copyFile, constants, mkdir, readdir, access } from 'node:fs';
+import path from 'path';
+const __dirname = import.meta.dirname;
+
 const copy = async () => {
-    // Write your code here 
+  const filesPath = path.join(__dirname, '/files');
+  const filesCopyPath = path.join(__dirname, '/files_copy');
+
+  access(filesPath, (err) => {
+    if (err.code === 'ENOENT') {
+      throw new Error('FS operation failed');
+    } else {
+      mkdir(filesCopyPath, { recursive: true }, (err) => {
+        if (err) {
+          throw err;
+        } else {
+          readdir(filesPath, { withFileTypes: true }, (err, files) => {
+            if (err) throw err;
+
+            for (let file of files) {
+              if (file.isFile()) {
+                copyFile(`${filesPath}/${file.name}`, `${filesCopyPath}/${file.name}`, constants.COPYFILE_EXCL, (err) => {
+                  if (err) {
+                    if (err.code === 'EEXIST') {
+                      throw new Error('FS operation failed')
+                    } else {
+                      throw err;
+                    }
+                  }
+                });
+              } else {
+                throw new Error(`${file.name} is not file`);
+              }
+            }
+          })
+        }
+      });
+    }
+  });
 };
 
 await copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
+import { appendFile, access } from 'node:fs';
+import path from 'path';
+const __dirname = import.meta.dirname;
+
 const create = async () => {
-    // Write your code here 
+  const filePath = path.join(__dirname, '/files/file.txt');
+  access(filePath, (err) => {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        appendFile(filePath, 'I am fresh and young', (err) => {
+          if (err) throw err;
+          console.log('file.txt created and "I am fresh and young" was appended to the file!');
+        });
+      }
+    } else {
+      throw new Error('FS operation failed');
+    }
+  });
 };
 
 await create();
