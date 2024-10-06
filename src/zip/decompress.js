5ea94d63709049a4859b71a5893308f8e23807c3
Kseniya <k.merkulova@iqdev.digital>
2024-10-06 23:50:10 +0500

    feat: add decompress function

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,22 @@
+import { pipeline } from 'node:stream';
+import zlib from 'node:zlib';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const filePath = join(__dirname, 'files', 'archive.gz');
+
 const decompress = async () => {
-    // Write your code here 
+  pipeline(
+    createReadStream(filePath),
+    zlib.createGunzip(),
+    createWriteStream(join(__dirname, 'files', 'fileToCompress.txt')),
+    (err) => {
+      err ? process.exitCode = 1 : console.log('Decompressed');
+    },
+  );
 };
 
 await decompress();
