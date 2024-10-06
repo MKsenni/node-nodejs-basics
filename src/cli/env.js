dd141cfe44e902e3d6e6d2b410d84dbf243cbae8
Kseniya <k.merkulova@iqdev.digital>
2024-10-06 22:05:45 +0500

    feat: add env function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
+import { env } from 'node:process';
+
 const parseEnv = () => {
-    // Write your code here 
+  const prefix = 'RSS_';
+  const envInConsole = Object.keys(env).filter((envKey) => envKey.includes(prefix)).map((key) => {
+    return `${key}=${env[key]}`
+  }).join('; ');
+  console.log(envInConsole);
 };
 
 parseEnv();
