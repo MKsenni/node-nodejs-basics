87807f681a5d162bd3e0f14558d546e945e6be6e
Kseniya <k.merkulova@iqdev.digital>
2024-10-07 00:26:20 +0500

    feat: add READ function

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,14 @@
+import { createReadStream } from 'node:fs';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const filePath = join(__dirname, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+  const readable = createReadStream(filePath).setEncoding('utf8');
+  readable.pipe(process.stdout);
 };
 
 await read();
