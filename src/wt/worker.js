825503833284d4e900effd3c6fabca9f7b833818
Kseniya <k.merkulova@iqdev.digital>
2024-10-07 22:48:31 +0500

    feat: add worker functions

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,24 @@
+import { Worker, workerData, parentPort, isMainThread } from 'node:worker_threads';
+import { fileURLToPath } from 'url';
+import { dirname } from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url);
+const random = Math.floor(Math.random()* (40 - 1) + 1);
+
+
 // n should be received from main thread
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
-const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+const sendResult = (n) => {
+  if (isMainThread) {
+    const worker = new Worker(__filename, { workerData: n });
+    worker.on('message', (result) => {
+      console.log(`Result of nthFibonacci(${n}) = ${result}`)
+    })
+  } else {
+    const result = nthFibonacci(workerData);
+    parentPort.postMessage(result);
+  }
 };
 
-sendResult();
+sendResult(random);
